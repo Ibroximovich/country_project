30629b67bf5025907859de088a09b9f08fd76f4e
Ibroximovich <azamovsarvar555@gmail.com>
2025-05-21 09:41:08 +0500

    country_project

---
+++ js/country.js
@@ -0,0 +1,165 @@
+// const countries = [
+//   {
+//     name: "Albania",
+//     population: 2886026,
+//     region: "Europe",
+//     capital: "Tirana",
+//     flag: "https://flagcdn.com/w320/al.png"
+//   },
+//   {
+//     name: "Brazil",
+//     population: 212559417,
+//     region: "Americas",
+//     capital: "Brasília",
+//     flag: "https://flagcdn.com/w320/br.png"
+//   },
+//   {
+//     name: "Canada",
+//     population: 38005238,
+//     region: "Americas",
+//     capital: "Ottawa",
+//     flag: "https://flagcdn.com/w320/ca.png"
+//   },
+//   {
+//     name: "Japan",
+//     population: 126476461,
+//     region: "Asia",
+//     capital: "Tokyo",
+//     flag: "https://flagcdn.com/w320/jp.png"
+//   },
+//   {
+//     name: "Germany",
+//     population: 83783942,
+//     region: "Europe",
+//     capital: "Berlin",
+//     flag: "https://flagcdn.com/w320/de.png"
+//   },
+//   {
+//     name: "Nigeria",
+//     population: 206139589,
+//     region: "Africa",
+//     capital: "Abuja",
+//     flag: "https://flagcdn.com/w320/ng.png"
+//   },
+//   {
+//     name: "Australia",
+//     population: 25499884,
+//     region: "Oceania",
+//     capital: "Canberra",
+//     flag: "https://flagcdn.com/w320/au.png"
+//   },
+//   {
+//     name: "India",
+//     population: 1380004385,
+//     region: "Asia",
+//     capital: "New Delhi",
+//     flag: "https://flagcdn.com/w320/in.png"
+//   },
+//   {
+//     name: "France",
+//     population: 65273511,
+//     region: "Europe",
+//     capital: "Paris",
+//     flag: "https://flagcdn.com/w320/fr.png"
+//   },
+//   {
+//     name: "Egypt",
+//     population: 102334404,
+//     region: "Africa",
+//     capital: "Cairo",
+//     flag: "https://flagcdn.com/w320/eg.png"
+//   }
+// ];
+
+const countries = [
+    {
+        id: 1,
+        name: "Wallis and Futuna",
+        capital: "Mata-Utu",
+        population: 11750,
+        flag: "https://flagcdn.com/wf.svg",
+        isLiked:false,
+        isBasket:false
+    },
+    {
+        id: 2,
+        name: "Iceland",
+        capital: "Reykjavik",
+        population: 366425,
+        flag: "https://flagcdn.com/is.svg",
+        isLiked:false,
+        isBasket:false
+    },
+    {
+        id: 3,
+        name: "Luxembourg",
+        capital: "Luxembourg",
+        population: 632275,
+        flag: "https://flagcdn.com/lu.svg",
+        isLiked:false,
+        isBasket:false
+    },
+    {
+        id: 4,
+        name: "Mali",
+        capital: "Bamako",
+        population: 20250834,
+        flag: "https://flagcdn.com/ml.svg",
+        isLiked:false,
+        isBasket:false
+    },
+    {
+        id: 5,
+        name: "Comoros",
+        capital: "Moroni",
+        population: 869595,
+        flag: "https://flagcdn.com/km.svg",
+        isLiked:false,
+        isBasket:false
+    },
+    {
+        id: 6,
+        name: "Australia",
+        capital: "Canberra",
+        population: 25687041,
+        flag: "https://flagcdn.com/au.svg",
+        isLiked:false,
+        isBasket:false
+    },
+    {
+        id: 7,
+        name: "Estonia",
+        capital: "Tallinn",
+        population: 1331057,
+        flag: "https://flagcdn.com/ee.svg",
+        isLiked:false,
+        isBasket:false
+    },
+    {
+        id: 8,
+        name: "Canada",
+        capital: "Ottawa",
+        population: 38005238,
+        flag: "https://flagcdn.com/ca.svg",
+        isLiked:false,
+        isBasket:false
+    },
+    {
+        id: 9,
+        name: "Belarus",
+        capital: "Minsk",
+        population: 9398861,
+        flag: "https://flagcdn.com/by.svg",
+        isLiked:false,
+        isBasket:false
+    },
+    {
+        id: 10,
+        name: "Guyana",
+        capital: "Georgetown",
+        population: 786559,
+        flag: "https://flagcdn.com/gy.svg",
+        isLiked:false,
+        isBasket:false
+    }
+]
---
+++ js/main.js
@@ -0,0 +1,112 @@
+ let elcountry = document.querySelector(".select")
+let createCountry = document.querySelector(".create-country")
+
+
+ function createSelectOption(){
+      countries.forEach(item =>{
+        let createOption = document.createElement("option")
+        createOption.textContent = item.capital
+        createOption.value = item.capital.toLowerCase()
+        elcountry.appendChild(createOption)
+      })
+ }
+
+ createSelectOption()
+
+ function renderProduct(arr,list){
+     list.innerHTML = null
+     arr.forEach(item =>{
+      let ellistItem= document.createElement("li")
+        ellistItem.className = "w-[264px] rounded-[5px] shadow-md shadow-grey-300"
+        list.appendChild(ellistItem)
+
+        ellistItem.innerHTML =`
+           <img class="!w-[260px] !h-[160px] object-cover" src="${item.flag}" alt="Country img" width="264" height="160px">
+            <div class="pl-[24px] py-[24px]">
+                <strong class="font-extra-bold text-[18px] leading-[28px] inline-block mb-[16px]">${item.name}</strong>
+                <p class="font-semibold text-[14px] leading-[16px]"> Population: <span class="font-light">${item.population}</span></p>
+                <p class="font-semibold text-[14px] leading-[16px] my-[8px]">Region: <span class="font-light">${item.region}</span></p>
+                <p class="font-semibold text-[14px] leading-[16px]"> Capital: <span class="font-light">${item.capital}</span></p>
+                
+            </div>
+              <div class="flex justify-between items-center pb-[20px] px-[24px] gap-[10px]">
+                 <button class="like-btn flex justify-center  w-[30%] flex items-center gap-[2px] border-[2px]  cursor-pointer  rounded-md p-2 duration-300  hover:bg-red-500 hover:border-none hover:text-white active:scale-90">
+                   Liked
+                </button>
+                <button class=" save-btn flex justify-center w-[30%] flex items-center gap-[5px] p-2 border-[2px] cursor-pointer  rounded-md  duration-300  hover:bg-blue-400 hover:border-none hover:text-white active:scale-90">
+                    Saved
+                </button>
+                <button  class="more-btn flex justify-center w-[30%] flex items-center border-[2px]  gap-[5px] rounded-md p-2 uration-300  duration-300 hover:bg-green-500 hover:border-none hover:text-white active:scale-90">
+                    More
+                </button>
+            </div>
+           `
+     })
+ }
+
+
+ renderProduct(countries,createCountry)
+
+ let mode = document.querySelector(".dark-mode")
+  let bt = document.querySelector(".bt")
+  let sv = document.querySelector(".save")
+
+  let btn = document.querySelectorAll(".like-btn").forEach((btn) =>{
+    btn.addEventListener("click", () =>{
+      bt.classList.toggle("fill-red-500")
+    })
+  })
+  let save = document.querySelectorAll(".save-btn").forEach((save) =>{
+    save.addEventListener("click", () => {
+      sv.classList.toggle("fill-blue-500")
+    })
+  })
+
+let mor = document.querySelectorAll(".more-btn");
+
+mor.forEach(item => {
+  item.addEventListener("click", () => {
+        alert("more")
+  });
+});
+
+    
+
+
+  const searchInput = document.querySelector("#search");
+
+  let searchselect = document.querySelector(".country")
+
+  searchselect.addEventListener("change",() =>{
+     let arr = countries.filter(item =>{
+       return item.capital.toLowerCase().includes(searchselect.value)
+     })
+     renderProduct(arr, createCountry)
+  })
+
+searchInput.addEventListener("input",() =>{
+  let search = countries.filter(item =>{
+    if(item.name.toLowerCase().trim().includes(searchInput.value)){
+      return  item.capital
+    }
+    else if(item.capital.toLowerCase().trim().includes(searchInput.value)){
+       return  item.capital
+    }
+   
+  })
+  renderProduct(search,createCountry)
+})
+
+
+
+
+
+let darkModeBtn = document.querySelector(".dark-mode1");
+
+darkModeBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+});
+
+
+
+
