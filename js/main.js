1a05a977f740c71d6d17a74fb4b3b5f58e6f5bba
Ibroximovich <azamovsarvar555@gmail.com>
2025-05-22 10:00:31 +0500

    update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
  let elcountry = document.querySelector(".select")
 let createCountry = document.querySelector(".create-country")
 
+ const searchInput = document.querySelector("#search");
+let searchselect = document.querySelector(".country")
+let countLiked = document.querySelector(".liked-count")
+let countsaved = document.querySelector(".saved-count")
+let bt = document.querySelector(".bt")
+
+
 
  function createSelectOption(){
       countries.forEach(item =>{
@@ -10,6 +17,7 @@ let createCountry = document.querySelector(".create-country")
         elcountry.appendChild(createOption)
       })
  }
+
 
  createSelectOption()
 
@@ -30,10 +38,10 @@ let createCountry = document.querySelector(".create-country")
                 
             </div>
               <div class="flex justify-between items-center pb-[20px] px-[24px] gap-[10px]">
-                 <button class="like-btn flex justify-center  w-[30%] flex items-center gap-[2px] border-[2px]  cursor-pointer  rounded-md p-2 duration-300  hover:bg-red-500 hover:border-none hover:text-white active:scale-90">
+                 <button onclick="handeLikeclick(${item.id})"  class="  ${item.isLiked ? "bg-red-500 text-white" : "border-slate-500"}  like-bt  flex justify-center  w-[30%] flex items-center gap-[2px] border-[2px]   cursor-pointer  rounded-md p-2 duration-300  ">
                    Liked
                 </button>
-                <button class=" save-btn flex justify-center w-[30%] flex items-center gap-[5px] p-2 border-[2px] cursor-pointer  rounded-md  duration-300  hover:bg-blue-400 hover:border-none hover:text-white active:scale-90">
+                <button onclick="handeSaveclick(${item.id})" class=" ${item.isBasket ? "bg-blue-500 text-white" : "border-slate-500"} flex justify-center w-[30%] flex items-center gap-[5px] p-2 border-[2px] cursor-pointer  rounded-md  duration-300  ">
                     Saved
                 </button>
                 <button  class="more-btn flex justify-center w-[30%] flex items-center border-[2px]  gap-[5px] rounded-md p-2 uration-300  duration-300 hover:bg-green-500 hover:border-none hover:text-white active:scale-90">
@@ -43,46 +51,21 @@ let createCountry = document.querySelector(".create-country")
            `
      })
  }
-
-
  renderProduct(countries,createCountry)
 
- let mode = document.querySelector(".dark-mode")
-  let bt = document.querySelector(".bt")
-  let sv = document.querySelector(".save")
 
-   document.querySelectorAll(".like-btn").forEach((btn) =>{
-    btn.addEventListener("click", () =>{
-      bt.classList.toggle("fill-red-500")
-    })
-  })
-   document.querySelectorAll(".save-btn").forEach((save) =>{
-    save.addEventListener("click", () => {
-      sv.classList.toggle("fill-blue-500")
-    })
-  })
-
-let mor = document.querySelectorAll(".more-btn");
-
-mor.forEach(item => {
-  item.addEventListener("click", () => {
-        alert("more")
-  });
-});
-
-    
-
-
-  const searchInput = document.querySelector("#search");
-
-  let searchselect = document.querySelector(".country")
 
   searchselect.addEventListener("change",() =>{
-     let arr = countries.filter(item =>{
-       return item.capital.toLowerCase().includes(searchselect.value)
-     })
-     renderProduct(arr, createCountry)
-  })
+      if(searchselect.value == "ol"){
+        renderProduct(countries,createCountry)
+      }
+      else{
+        let arr = countries.filter(item =>{
+          return item.capital.toLowerCase().includes(searchselect.value)
+        })
+        renderProduct(arr, createCountry)
+      }
+    })
 
 searchInput.addEventListener("input",() =>{
   let search = countries.filter(item =>{
@@ -96,6 +79,38 @@ searchInput.addEventListener("input",() =>{
   })
   renderProduct(search,createCountry)
 })
+
+function handeLikeclick(id){
+ let findObject = countries.find(item => item.id == id)
+ findObject.isLiked = !findObject.isLiked
+ renderProduct(countries,createCountry)
+ countLiked.textContent = countries.filter(item => item.isLiked == true).length
+
+}
+
+function  handbtnclick(){
+  let arr = countries.filter(item => item.isLiked ==true)
+  renderProduct(arr,createCountry)
+}
+
+function handeSaveclick(id){
+ let findObject = countries.find(item => item.id == id)
+ findObject.isBasket = !findObject.isBasket
+ renderProduct(countries,createCountry)
+ countsaved.textContent = countries.filter(item => item.isBasket == true).length
+}
+
+function  handsvdclick(){
+  let arr = countries.filter(item => item.isBasket ==true)
+  renderProduct(arr,createCountry)
+}
+
+
+
+
+
+
+
 
 
 
